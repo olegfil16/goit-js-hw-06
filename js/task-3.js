8bef665de29fac689e54ad83761ce5861e2a856a
Oleg Fiyalkovych <olegfil1608@gmail.com>
2024-09-29 13:50:17 +0200

    nove

---
+++ js/task-3.js
@@ -0,0 +1,29 @@
+class StringBuilder {
+  #value;
+  constructor(value) {
+    this.#value = value;
+  }
+  getValue() {
+    return this.#value;
+  }
+  padStart(str) {
+    this.#value = str.concat(this.#value);
+  }
+
+  padEnd(str) {
+    this.#value = this.#value.concat(str);
+  }
+
+  padBoth(str) {
+    this.#value = str.concat(this.#value).concat(str);
+  }
+}
+const builder = new StringBuilder('.');
+
+console.log(builder.getValue()); // "."
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
